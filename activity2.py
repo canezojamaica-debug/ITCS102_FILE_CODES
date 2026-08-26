bf064a7ef0f9ee7ce802acc1981bce5321e44786
canezojamaica-debug <canezojamaica@gmail.com>
2026-08-26 19:10:37 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,4 @@
+#My very first program
+
+
+print("Hello World!")
---
+++ activity3.py
@@ -0,0 +1,35 @@
+#using variables
+
+#int 
+
+age = 18
+weight = 60
+height = 145
+seatmates = 4
+
+#float
+
+temperature = 28.5
+speed = 39.2
+average = 93.9
+pi = 3.14
+
+#string
+
+username = "Jamaica D. Cañezo"
+password = "this_is_xiv"
+course = "BSIT 1B"
+school = "Dalubhasaan ng Lungsod ng Lucena"
+
+print("Hi, my name's", username,"I'm currently", age,"years old", "from", course, "of", school, "and here are data about me")
+
+print("age=", age)
+print("weight =", weight)
+print("height =", height)
+print("seatmates =",seatmates)
+print("temperature =", temperature)
+print("speed =", speed)
+print("average =",average)
+print("pi =",pi)
+print("username =", username)
+print("password =", password)
