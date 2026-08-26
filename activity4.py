9a5e3cbb9e93d15c75e629ebf5a74e673da01a61
canezojamaica-debug <canezojamaica@gmail.com>
2026-08-26 20:11:10 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#escape sequence
+
+# \n - new line 
+# \t - tab space
+# \r - carriage return / deprecated (obsolete)
+# \b - backspace
+# \\ - insert slash
+# \* - insert double quotation
+
+phrase1 = "\tChange is the only constant thing in this world, \n\t I know what you pretend I am, \n\t I know who I pretend to be."
+
+print(phrase1)
