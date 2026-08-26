0135b30d18536fdd1558e6803497c8ff05deedc9
canezojamaica-debug <canezojamaica@gmail.com>
2026-08-26 19:38:58 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,15 @@
+a = 19
+b = 5
+sum = a+b
+diff = a-b
+prod = a*b
+quo = a/b
+
+
+print("The sum of",a,"and",b,"is",sum)
+print("The difference of", a, "and", b, "is", diff)
+print("the product of", a, "and", b, "is", prod)
+print("The quotient of",a,"and",b,"is", quo)
+print("The value of", a,"raised to",b,"is", a**b)
+print("The quotient of", a, "divided by", b, "floor division",a//b)
+print( a, "modulo", b,"is equal to", a%b)
