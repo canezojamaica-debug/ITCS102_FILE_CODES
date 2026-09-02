d1e4c9c8ea7814921388de4767196ac59bb0ce8f
canezojamaica-debug <canezojamaica@gmail.com>
2026-09-02 12:18:09 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,14 +1,45 @@
-money_to_deposit = 11935
-
-ath = money_to_deposit // 1000
-afh = money_to_deposit % 1000 // 500
-atwh = money_to_deposit %1000 %500 // 200
-aoh = money_to_deposit % 1000 % 500 % 200 // 100
-aft = money_to_deposit %1000 % 500 % 200 % 100 //50	
-atw = money_to_deposit %1000 %500 %200 %100 %50 // 20
-at = money_to_deposit %1000 %500 %200 %100 %50 % 20 // 10
-af = money_to_deposit % 1000 % 500 % 200 %100 % 50 % 20 % 10 // 5
-ao = money_to_deposit % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5 // 1
+money = eval(input("Enter Money to DEPOSIT ----->>>"))
+print(type(money))
+print("================================= PH BANK DENOMINATION ====================================== ")
+print("MONEY TO DEPOSIT ---------------------> ", money, "php")
 
 
-print("The amount of",money_to_deposit,"pesos consists of",ath,"one-thousand peso bills",afh,"five-hundred peso bill",atwh,"two-hundred peso bills",aoh,"one-hundred peso bill",aft,"fifty peso bill",atw,"twenty peso bill",at,"peso coin",af,"peso coin",ao,"peso coin.")
+oth = money // 1000
+oth_change = money % 1000
+
+fvh = oth_change // 500
+fvh_change = oth_change % 500
+
+twh = fvh_change // 200
+twh_change = fvh_change % 200 
+
+oneh = twh_change // 100
+oneh_change = twh_change % 100
+
+fty = oneh_change // 50
+fty_change = oneh_change % 50 
+
+tty = fty_change // 20
+tty_change = fty_change % 20
+
+ten = tty_change // 10
+ten_change = tty_change % 10
+
+five = ten_change // 5
+five_change = ten_change % 5
+
+piso = five_change 
+
+print()
+print("\n\t1000 -", oth)
+print("\t500 -",fvh)
+print("\t200 -",twh)
+print("\t100 -",oneh)
+print("\t50 -",fty)
+print("\t20 -",tty)
+print("\t10 -",ten)
+print("\t5 -" , five)
+print("\t1 -", piso)
+
+
+print("===================================== END OF DENOMINATION =======================================")
