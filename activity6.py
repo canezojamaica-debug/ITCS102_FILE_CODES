e638af83f3f0a5b39b750691d52d2f3010dd4def
canezojamaica-debug <canezojamaica@gmail.com>
2026-09-02 11:36:34 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,9 @@
+name = input("What is your name? -->")
+
+print("Hello", name,"!","What's up?")
+
+age = input("How old are you? -->")
+
+print("Ohh so you're currently", age,"years old, that's nice!")
+
+print("take care, bye!")
