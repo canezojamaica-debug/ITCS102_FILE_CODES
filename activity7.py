5a268032a9841b9075235f68b9b8d6239da89226
canezojamaica-debug <canezojamaica@gmail.com>
2026-09-06 13:07:57 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,35 @@
+total = 30 
+
+print(type)
+ 
+a = eval(input("enter any number --->"))
+total = total + a
+
+b = eval(input("enter any number --->"))
+total = total - b
+
+c = eval(input("enter any number --->"))
+total = total * c
+
+d = eval(input("enter any number --->"))
+total = total / d
+
+e = eval(input("enter any number --->"))
+total += e
+
+f = eval(input("enter any number --->"))
+total -= f
+
+g = eval(input("enter any number --->"))
+total *= g 
+
+h = eval(input("enter any number --->"))
+total /= h
+
+x =  eval(input("enter any number --->"))
+total // x
+
+v =  eval(input("enter any number --->"))
+total % v
+
+print("current total is --->",total)
